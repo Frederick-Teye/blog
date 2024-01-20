afe53622dddcee3c5ed820007829648d04a98ad6
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-01-20 08:31:40 +0000

    forms and user accounts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -18,3 +20,21 @@ class BlogDetailView(DetailView):
 # with the above edits, we can use blog_detail as the context object name
 # so that will override the context object name naturally provided by django for us
 # which is #object or #post(lower case of the db table name)
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body", "author"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
